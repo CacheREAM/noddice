d1399fd69ba45d990dc737e5c9fb2e83e11ab29d
Jeremy <jeremypeevpe@gmail.com>
2025-06-21 14:57:41 -0700

    slash commands

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+import discord
 from discord.ext import commands
 import random
 from keys import TOKEN
 
-bot = commands.Bot(command_prefix='!')
+intents = discord.Intents.default()
+intents.typing = False
+intents.presences = False
+
+bot = commands.Bot(command_prefix=None, intents=intents)
 
 
 def roll_dice(n):
@@ -21,15 +26,16 @@ def roll_dice(n):
 @bot.event
 async def on_ready():
     print(f'{bot.user} has connected to Discord!')
+    await bot.tree.sync()
 
 
-@bot.command(name='roll')
+@bot.slash_command(name='roll', description='Rolls a dice')
 async def roll(ctx, n: int):
     if n <= 0:
-        await ctx.send('You must roll at least one die.')
+        await ctx.respond('You must roll at least one die.')
         return
     results = roll_dice(n)
     fives_and_sixes = [roll for roll in results if roll >= 5]
-    await ctx.send(f'You rolled: {results}\nSuccesses: {sum(fives_and_sixes)}')
+    await ctx.respond(f'You rolled: {results}\nSuccesses: {sum(fives_and_sixes)}')
 
 bot.run(TOKEN)
